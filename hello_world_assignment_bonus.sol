70106a0f5cf0ebc4772357921491a075830b1949
hotgary67 <69466703+hotgary67@users.noreply.github.com>
2020-08-31 21:22:29 +0200

    Create hello_world_assignment_bonus.sol
    bonus assignment count number of People added by Ethereum address

---
+++ hello_world_assignment_bonus.sol
@@ -0,0 +1,46 @@
+pragma solidity 0.5.12;
+pragma experimental ABIEncoderV2;
+
+contract HelloWorld{
+
+    struct Person {
+      string name;
+      uint age;
+      uint height;
+      bool senior;
+      address creator;
+
+    }
+
+    mapping (address => uint ) public addressCounter;
+
+
+
+    Person[] public people;
+
+    function createPerson(string memory name, uint age, uint height) public {
+
+        address creator = msg.sender;
+        //This creates a person
+        Person memory newPerson;
+        newPerson.name = name;
+        newPerson.age = age;
+        newPerson.height = height;
+        newPerson.creator = creator;
+
+        if(age >= 65){
+           newPerson.senior = true;
+           }
+        else{
+           newPerson.senior = false;
+       }
+
+       people.push(newPerson);
+         addressCounter[creator] += 1;
+    }
+
+
+    function getPerson(uint index) public view returns(Person memory)    {
+         return people[index];
+        }
+}
